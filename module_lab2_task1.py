08076ba2a28a24d51decff275dfeb01cb47d6679
celinebuiiii <127638310+celinebuiiii@users.noreply.github.com>
2023-03-27 17:44:36 +1300

    fixed read file method + unit tests

--- module_lab2_task1.py
+++ module_lab2_task1.py
@@ -18,23 +18,27 @@ class LUSolver(object):
                f'Ly = b gives a y-vector of: {self.vector_y}' \
                f'Ux = y gives a solution x-vector of: {self.vector_x}'
 
+    # method 1
     def read_system_from_file(self, f_path):
         with open(f_path, 'r') as fp:
             # get number of unknowns
-            n = fp.readline()
+            n = int(fp.readline().strip())
+            self.matrix_a = np.zeros((n, n), dtype='int32')
+            self.vector_b = np.zeros((n, 1), dtype='int32')
             line = fp.readline().strip()
 
-            # loop thru the next n lines to get matrix A
-            count = 1
-            while count <= n:
-                a_values = line.split(',')
-                self.matrix_a.append(float(a_values))
-                count += 1
+            # loop through the next n lines to get matrix A
+            count = 0
+            while line != '':
+                if count < n:
+                    a_values = line.split(',')
+                    self.matrix_a[count] = np.array(a_values)
+                    count += 1
+
+                else:
+                    b_vector = line.strip()
+                    self.vector_b[count-n] = np.array(b_vector)
+                    count += 1
+
                 line = fp.readline().strip()
 
-            # after looping for matrix A finishes
-            # loop thru the rest till end line
-            while line != '':
-                b_values = line.strip()
-                self.vector_b.append(float(b_values))
-                line = fp.readline().strip()
--- test_lusolver.py
+++ test_lusolver.py
@@ -1,7 +1,20 @@
 import pytest
+import os
+import numpy as np
 from module_lab2_task1 import *
-from problems import *
 
 
-def test_read_system_from_file():
-    problem = LUSolver.read_system_from_file()
+# test for vector b
+def test_read_system_from_file1():
+    solver = LUSolver()
+    solver.read_system_from_file(r'/Users/celinebui/Desktop/engsci233_lab2/233_lab2/problems/problem0.txt')
+    b_test = [[-5], [20], [3]]
+    assert (np.array(solver.vector_b).all() == np.array(b_test).all())
+
+
+# test for matrix A
+def test_read_system_from_file2():
+    solver = LUSolver()
+    solver.read_system_from_file(r'/Users/celinebui/Desktop/engsci233_lab2/233_lab2/problems/problem0.txt')
+    matrix_a_test = [[2, -1, 3], [-8, 3, -8], [-2, -2, 7]]
+    assert (np.array(solver.matrix_a).all() == np.array(matrix_a_test).all())
