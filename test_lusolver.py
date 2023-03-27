0b5ffe5253e41e80724c3e584d31d77aae7ec7fb
Blakesbbb <127801542+Blakesbbb@users.noreply.github.com>
2023-03-28 00:25:48 +1300

    Done test for forward substitution
    test for forward substitution added

--- test_lusolver.py
+++ test_lusolver.py
@@ -19,10 +19,20 @@ def test_read_system_from_file2():
     matrix_a_test = [[2, -1, 3], [-8, 3, -8], [-2, -2, 7]]
     assert (np.array(solver.matrix_a).all() == np.array(matrix_a_test).all())
 
+
 def test_backward_sub():
     tester = LUSolver()
     tester.matrix_u = np.array([[2, -1, 3], [0, -1, 4], [0, 0, -2]])
     tester.vector_y = np.array([-5, 0, -2])
     tester.backward_sub()
-    x_testvalue = np.array([-2,4,1])
+    x_testvalue = np.array([-2, 4, 1])
     assert (np.array(tester.vector_x).all() == x_testvalue.all())
+
+
+def test_forward_sub():
+    solver = LUSolver()
+    solver.matrix_l = np.array([[1, 0, 0], [-4, 1, 0], [-1, 3, 1]])
+    solver.vector_b = np.array([-5, 20, 3])
+    solver.forward_sub()
+    correct_y = np.array([[-5, 0, -2]])
+    assert (np.array(solver.vector_y).all() == correct_y.all())
