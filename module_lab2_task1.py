24cb70fde15d0759169859e3418f9fb62a906d88
Jcan156 <jcan156@aucklanduni.ac.nz>
2023-03-27 20:04:28 +1300

    did method 4 and test :)

--- module_lab2_task1.py
+++ module_lab2_task1.py
@@ -1,4 +1,6 @@
 import numpy as np
+
+
 # additional imports here
 
 
@@ -31,3 +33,34 @@ class LUSolver(object):
                 b_values = line.strip()
                 self.vector_b.append(float(b_values))
                 line = fp.readline().strip()
+
+    def backward_sub(self):
+        """
+        Calculates the solutions to the x vector using backwards substitution (Ux = y)
+
+        --------
+
+        Notes:
+        Does not have any inputs or outputs, it just updates the vector x attribute.
+
+    `   """
+
+        # self.vector_x = np.linalg.solve(self.matrix_u, self.vector_y)
+
+        size = len(self.matrix_u) - 1
+        # Assigning the x vector to all zeros in order to do calculations with it
+        self.vector_x = np.zeros((size + 1, 1))
+
+        # For loop to index every row
+        for i in range(0, size + 1):
+            j = 0
+            sum = 0
+            # While loop multiplies every previously calculated x value with its corresponding u matrix value in
+            # order to calculate the next x value. The loop breaks when the next u matrix value is a 0 or its reached
+            # the end of the row.
+            while (self.matrix_u[size - i, size - 1 - j] != 0) and (j != size):
+                sum = sum + self.vector_x[size - j] * self.matrix_u[size - i, size - j]
+                j = j + 1
+            # Finally, the x value is calculated by summing the previous while loop sum and the y value for that
+            # row. Then everything is divided by the u matrix value for the corresponding x variable.
+            self.vector_x[size - i] = (sum + self.vector_y[size - i]) / self.matrix_u[size - i, size - j]
