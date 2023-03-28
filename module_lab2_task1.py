be3d9cb2710163855bbc37516f9914907286be8e
Blakesbbb <127801542+Blakesbbb@users.noreply.github.com>
2023-03-29 00:02:08 +1300

    Fixed Forward substitution

--- module_lab2_task1.py
+++ module_lab2_task1.py
@@ -132,20 +132,20 @@ class LUSolver(object):
         size = len(self.matrix_l)
 
         # vector_y is an array of zeros of size+1 rows and 1 column
-        self.vector_y = np.zeros((size, 1))
+        self.vector_y = np.zeros(size, 1)
 
         # nested for loop that calculates the values of each y and puts them into the vector_y
         # array replacing the zeros
         for i in range(size):
             sum = 0
 
-            for j in range(size-1):
+            for j in range(i):
                 # sum is a variable that stores the sum of the products of the matrix_l and
                 # the corresponding values of the vector_y
-                sum = sum + (self.matrix_l[i, j] * self.vector_y[j])
+                sum = sum + self.matrix_l[i, j] * self.vector_y[j]
 
                 # calculates the value for each row of vector_y
-                self.vector_y[i] = (self.vector_b[i] - sum) / self.matrix_l[i, i]
+            self.vector_y[i] = (self.vector_b[i] - sum) / self.matrix_l[i, i]
 
     def write_solution_to_file(self, f_path):
         with open(f_path, 'w') as fp:
