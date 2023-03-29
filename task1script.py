4e3c6d56e70c26b1f610bfb1229b7601d165f258
Jcan156 <127263690+Jcan156@users.noreply.github.com>
2023-03-29 13:55:34 +1300

    new script

--- task1script.py
+++ task1script.py
@@ -20,7 +20,7 @@ for match in glob(os.path.join(currentDirectory,'problems','problem*')):
     counterString = str(counter)
     solver = LUSolver()
     solver.read_system_from_file(match)
-    solver.lu_factors()
+    solver.lu_solver()
     solver.forward_sub()
     solver.backward_sub()
     solver.write_solution_to_file(os.path.join(currentDirectory,'solutions','solutions'+counterString+'.txt'))
