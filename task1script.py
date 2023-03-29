162ecb63fd68047f66b92e8c733ac539a4677120
Jcan156 <127263690+Jcan156@users.noreply.github.com>
2023-03-29 14:40:09 +1300

    with correct while loop ;)

--- task1script.py
+++ task1script.py
@@ -5,8 +5,6 @@ import numpy as np
 from module_lab2_task1 import *
 from glob import glob
 
-import os
-
 try:
     os.makedirs('solutions')
 except OSError:
@@ -15,17 +13,19 @@ except OSError:
 currentDirectory = os.getcwd()
 
 counter = 0
+match = glob(os.path.join(currentDirectory,'problems',f'problem{counter}*'))
 
-while counter < 101:
-    for match in glob(os.path.join(currentDirectory,'problems',f'problem{counter}*')):
-        counterString = str(counter)
-        solver = LUSolver()
-        solver.read_system_from_file(match)
-        solver.lu_solver()
-        solver.forward_sub()
-        solver.backward_sub()
-        solver.write_solution_to_file(os.path.join(currentDirectory,'solutions','solutions'+counterString+'.txt'))
+while match != []:
+    match = match[0]
+    counterString = str(counter)
+    solver = LUSolver()
+    solver.read_system_from_file(match)
+    solver.lu_solver()
+    solver.forward_sub()
+    solver.backward_sub()
+    solver.write_solution_to_file(os.path.join(currentDirectory,'solutions','solutions'+counterString+'.txt'))
     counter = counter + 1
+    match = glob(os.path.join(currentDirectory,'problems',f'problem{counter}*'))
 
 
 
